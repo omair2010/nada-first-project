53a3923dbfd6aa43df141b5374c711419b00e606
omar-bear <omar@bearstudio.fr>
2020-05-10 12:55:25 +0100

    add movies

--- src/App.js
+++ src/App.js
@@ -1,32 +1,43 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import "./App.css";
+import { getMovies, preUrl } from "./services/movies";
 
 const App = () => {
+  const [movies, setMovies] = useState([]);
 
-  const [inputValue, setInputValue] = useState("");
+  const chargeMovies = useCallback(async () => {
+    const res = await getMovies();
+    if (res && res.data && res.data.results) {
+      console.log(res.data.results);
+      setMovies(res.data.results);
+    }
+  }, []);
 
-  const [array, setArray] = useState([]);
+  useEffect(() => {
+    chargeMovies();
+  }, [chargeMovies]);
 
-  const changeInputValue = (e) => setInputValue(e.target.value);
-
-  const handleClickGurd = () => {
-    setArray([...array, inputValue]);
-    setInputValue("");
-  };
-  
-  return (
-    <div className="App">
-      <div>
-        <input value={inputValue} onChange={changeInputValue} />
-        <button onClick={handleClickGurd}>Ajouter</button>
-      </div>
-      {array.map((ele) => (
-        <div style={{ display: "flex", flexDirection: "column" }}>
-          <b>{ele}</b>
+  const renderShowMovies = () =>
+    movies.map((movie) => (
+      <div key={movie.id}>
+        <div style={{ fontWeight: "bold", marginTop: 20, marginBottom: 10 }}>
+          {" "}
+          {movie.original_title}{" "}
         </div>
-      ))}
-    </div>
-  );
+        <div>
+          <img
+            style={{ width: 400 }}
+            src={preUrl + movie.poster_path}
+            alt="imagenotworking"
+          />
+        </div>
+        <div style={{ fontWeight: "bold", color: 'blue', marginTop: 20, marginBottom: 10 }}>
+          {movie.overview}
+        </div>
+      </div>
+    ));
+
+  return <div className="App">{renderShowMovies()}</div>;
 };
 
 export default App;
